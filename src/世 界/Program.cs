046af8d727862bfda3fa562c7729977f8354a1b3
宿梓航 <Ferdinandsukhoi@outlook.com>
2020-06-04 18:06:13 +0800

    Init.Commit

---
+++ src/世 界/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace 世_界
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ごめん┙");
+            Thread.Sleep(1000);
+            for (var i = 0; i < 80; i++)
+            {
+                Console.WriteLine("┙");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("さよなら");
+            Thread.Sleep(10000);
+        }
+    }
+}
